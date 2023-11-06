004b0f28e7f6d5d16104404879ba0ee8d076da95
Georgiy <georgii_sidenko@mail.ru>
2023-11-07 02:05:21 +0300

    added chat

--- main.py
+++ main.py
@@ -1,32 +1,25 @@
 import datetime
 
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 from model.db import register_user, login_user
 
 app = Flask(__name__, static_folder='static')
-app.secret_key = 'qwerty'
+app.secret_key = 'key'
 app.permanent_session_lifetime = datetime.timedelta(minutes=10)
 socketio = SocketIO(app)
 
-users = {
-    "User1": {"sid": "1234"},
-    "User2": {"sid": "4321"},
-}
 
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def get_home():
     return render_template('home.html')
 
 
-@socketio.on("message")
-def on_message(data):
-    user = users[data["user"]]
-    socketio.emit("message", {
-        "user": user["sid"],
-        "text": data["text"],
-    }, to=user["sid"])
+@socketio.on("new_message")
+def handle_new_message(message):
+    print(request.sid)
+    print(f"New message: {message}")
+    emit("chat", {"message": message, "username": request.sid}, broadcast=True)
 
 
 @app.route('/profile/<message>', methods=['GET'])
