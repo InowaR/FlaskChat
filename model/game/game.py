a50b4f346a112d433e013e1583c6ec1a97591386
georgiy <georgii_sidenko@mail.ru>
2024-01-30 05:10:23 +0300

    fix

--- model/game/game.py
+++ model/game/game.py
@@ -7,12 +7,15 @@ class Game:
     def __init__(self):
         # self.game_id = random.randint(1, 10)
         self.game_id = 1
+        self.start_blind = False
+        self.start_preflop = False
+        self.start_flop = False
+        self.start_turn = False
+        self.start_river = False
         self.list_players = []
         self.deck = []
         self.table_cards = []
         self.table_money = 0
-        self.blind1 = 100
-        self.blind2 = 200
         self.round = 0
 
     def check_group_round(self) -> int:
@@ -22,18 +25,22 @@ class Game:
         for player in self.list_players:
             if player.name == player_name:
                 if button == 'fold':
-                    if round_number == 0:
+                    if int(round_number) == 0:
                         self.round += 1
                         player.play_preflop = True
-                    if round_number == 1:
+                        print('player preflop = True')
+                    if int(round_number) == 1:
                         self.round += 1
                         player.play_flop = True
-                    if round_number == 2:
+                        print('player flop = True')
+                    if int(round_number) == 2:
                         self.round += 1
                         player.play_turn = True
-                    if round_number == 3:
+                        print('player turn = True')
+                    if int(round_number) == 3:
                         self.round += 1
                         player.play_river = True
+                        print('player river = True')
 
     def check_player_buttons(self, player_name: str) -> tuple:
         for player in self.list_players:
@@ -67,28 +74,44 @@ class Game:
                 player.hand.append(self.deck.pop())
 
     def blind(self) -> tuple:
-        p1 = self.list_players[0]
-        p2 = self.list_players[1]
-        p1.money -= self.blind1
-        p2.money -= self.blind2
-        self.table_money += (self.blind1 + self.blind2)
-        return self.blind1, self.blind2
+        blind1 = 100
+        blind2 = 200
+        if not self.start_blind:
+            p1 = self.list_players[0]
+            p2 = self.list_players[1]
+            p1.money -= blind1
+            p2.money -= blind2
+            self.table_money += (blind1 + blind2)
+            self.start_blind = True
+            return blind1, blind2
+        return blind1, blind2
 
     def preflop(self) -> None:
-        self.create_deck()
-        self.deal_cards()
+        if not self.start_preflop:
+            self.create_deck()
+            self.deal_cards()
+            self.start_preflop = True
 
     def flop(self) -> list:
-        for _ in range(3):
-            self.table_cards.append(self.deck.pop())
+        if not self.start_flop:
+            for _ in range(3):
+                self.table_cards.append(self.deck.pop())
+            self.start_flop = True
+            return self.table_cards
         return self.table_cards
 
     def turn(self) -> list:
-        for _ in range(1):
-            self.table_cards.append(self.deck.pop())
+        if not self.start_turn:
+            for _ in range(1):
+                self.table_cards.append(self.deck.pop())
+            self.start_turn = True
+            return self.table_cards
         return self.table_cards
 
     def river(self) -> list:
-        for _ in range(1):
-            self.table_cards.append(self.deck.pop())
+        if not self.start_river:
+            for _ in range(1):
+                self.table_cards.append(self.deck.pop())
+            self.start_river = True
+            return self.table_cards
         return self.table_cards
